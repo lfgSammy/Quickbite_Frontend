c2e60d4d3d3c792ba896f2b74bee9f1fc0b3a37c
lfgSammy <sammylovesdesign@gmail.com>
2026-09-07 10:29:25 +0100

    feat: update API endpoints to remove 'user' prefix for consistency

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,49 +1,49 @@
-import client from './client';
+import client, { unwrapList } from './client';
 
 export function register(data) {
-  return client.post('/user/auth/register/', data).then((res) => res.data);
+  return client.post('/auth/register/', data).then((res) => res.data);
 }
 
 export function login(data) {
-  return client.post('/user/auth/login/', data).then((res) => res.data);
+  return client.post('/auth/login/', data).then((res) => res.data);
 }
 
 export function googleAuth(data) {
-  return client.post('/user/auth/google/', data).then((res) => res.data);
+  return client.post('/auth/google/', data).then((res) => res.data);
 }
 
 export function forgotPassword(data) {
-  return client.post('/user/auth/forgot-password/', data).then((res) => res.data);
+  return client.post('/auth/forgot-password/', data).then((res) => res.data);
 }
 
 export function verifyResetOtp(data) {
-  return client.post('/user/auth/verify-reset-otp/', data).then((res) => res.data);
+  return client.post('/auth/verify-reset-otp/', data).then((res) => res.data);
 }
 
 export function resetPassword(data) {
-  return client.post('/user/auth/reset-password/', data).then((res) => res.data);
+  return client.post('/auth/reset-password/', data).then((res) => res.data);
 }
 
 export function getProfile() {
-  return client.get('/user/auth/profile/').then((res) => res.data);
+  return client.get('/auth/profile/').then((res) => res.data);
 }
 
 export function updateProfile(data) {
-  return client.patch('/user/auth/profile/', data).then((res) => res.data);
+  return client.patch('/auth/profile/', data).then((res) => res.data);
 }
 
 export function getRestaurantStatus() {
-  return client.get('/user/restaurant/status/').then((res) => res.data);
+  return client.get('/restaurant/status/').then((res) => res.data);
 }
 
 export function getOperatingHours() {
-  return client.get('/user/restaurant/hours/').then((res) => res.data);
+  return client.get('/restaurant/hours/').then((res) => res.data);
 }
 
 export function getNotifications() {
-  return client.get('/user/notifications/').then((res) => res.data);
+  return client.get('/notifications/').then((res) => unwrapList(res.data));
 }
 
 export function markNotificationsRead() {
-  return client.patch('/user/notifications/').then((res) => res.data);
+  return client.patch('/notifications/').then((res) => res.data);
 }
