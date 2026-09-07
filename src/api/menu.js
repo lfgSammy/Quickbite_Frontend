8503d6e4e30aed843c14ac5e0e7d9953f7609b21
lfgSammy <sammylovesdesign@gmail.com>
2026-09-07 10:31:26 +0100

    feat: update API endpoints to remove redundant 'menu' prefix for clarity

--- src/api/menu.js
+++ src/api/menu.js
@@ -1,25 +1,25 @@
 import client from './client';
 
 export function getMenuItems() {
-  return client.get('/menu/menu/').then((res) => res.data);
+  return client.get('/menu/').then((res) => res.data);
 }
 
 export function getMenuItem(id) {
-  return client.get(`/menu/menu/${id}/`).then((res) => res.data);
+  return client.get(`/menu/${id}/`).then((res) => res.data);
 }
 
 export function getRiceTypes() {
-  return client.get('/menu/rice-types/').then((res) => res.data);
+  return client.get('/rice-types/').then((res) => res.data);
 }
 
 export function getRiceExtras() {
-  return client.get('/menu/rice-extras/').then((res) => res.data);
+  return client.get('/rice-extras/').then((res) => res.data);
 }
 
 export function getShawarmaExtras() {
-  return client.get('/menu/shawarma-extras/').then((res) => res.data);
+  return client.get('/shawarma-extras/').then((res) => res.data);
 }
 
 export function getDrinks() {
-  return client.get('/menu/drinks/').then((res) => res.data);
+  return client.get('/drinks/').then((res) => res.data);
 }
